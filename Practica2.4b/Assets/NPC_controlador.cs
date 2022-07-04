643a068c11d824bb6f0ef97ca664da098e61ff79
Jose Carlos Jiménez <35237294+Carlostart@users.noreply.github.com>
2022-07-04 14:34:58 +0200

    Add files via upload

---
+++ Practica2.4b/Assets/NPC_controlador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NPC_controlador : MonoBehaviour{
+    public NavMeshAgent miAgente;
+    public GameObject[] puntosCamino;
+    public int objetivoActual;
+
+    void Start () {
+        if (miAgente==null) miAgente = GetComponent<NavMeshAgent>();
+        miAgente.SetDestination (puntosCamino[0].transform.position);
+    }
+
+    void Update(){
+        if (miAgente.remainingDistance <= miAgente.stoppingDistance){
+            objetivoActual++;
+            if (objetivoActual >= puntosCamino.Length) objetivoActual = 0;
+            miAgente.destination = puntosCamino[objetivoActual].transform.position;
+        }
+    }
+}
