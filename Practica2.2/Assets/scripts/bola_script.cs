bab39a46eae8776e6927826d5b30574663911f56
Jose Carlos Jiménez <35237294+Carlostart@users.noreply.github.com>
2022-07-04 14:09:20 +0200

    Add files via upload

---
+++ Practica2.2/Assets/scripts/bola_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cuboEnemigo;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 p1 = transform.position;
+        Vector3 p2 = cuboEnemigo.transform.position;
+        if(Vector3.Distance(p1,p2) < 2)
+            Debug.Log("enemigo cerca!");
+    }
+}
---
+++ Practica2.2/Assets/scripts/control_cubo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control_cubo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(h,0,0);
+    }
+}
